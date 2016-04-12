6bccdfede9bd31c48710e647d463e52180c50495
Mark Christian D. Lopez <xmarkclx@gmail.com>
2016-04-12 21:24:07 +0800

    Better logging interface

--- EasyLogger.php
+++ EasyLogger.php
@@ -1,42 +1,76 @@
 <?php
 
+/**
+ * Class EasyLogger
+ * @author Mark Christian D. Lopez <xmarkclx@gmail.com>
+ */
 class EasyLogger{
-    #TODO: Better microseconds support http://stackoverflow.com/questions/169428/php-datetime-microseconds-always-returns-0
-    public static $log_file;
+    private static $showCallerAndLines = true;
+
+    /** @var EasyLogger $logger */
+    private static $logger;
     private $file;
 
-    function __construct($log_file = 'notification.log'){
-        // Open file
-        $this->file = fopen(BASE_PATH.'/'.$log_file, 'a');
+    function __construct($logFile = 'notification.log'){
+        $this->file = fopen(BASE_PATH . '/' . $logFile, 'a');
     }
 
-    public static function logThisVariable($var){
-        EasyLogger::logThis(var_export($var, true));
+    private static function instantiate(){
+        if(!isset(EasyLogger::$logger)) {
+            EasyLogger::$logger = new EasyLogger();
+        }
     }
 
-    public static function serializeThisVariable($var){
-        EasyLogger::logThis(serialize($var));
+    public static function quickLog($message){
+        EasyLogger::instantiate();
+
+        if(EasyLogger::$showCallerAndLines) {
+            $bt = debug_backtrace();
+            $caller = array_shift($bt);
+            $debugging = basename($caller['file']) . ':' . $caller['line'];
+            EasyLogger::$logger->log($debugging);
+        }
+        EasyLogger::$logger->log($message);
     }
 
-    public static function logThis($message){
-        $log_file = 'notification.log';
-        $file = fopen(BASE_PATH.'/'.$log_file, 'a');
-        $date = new DateTime('now');
-        $txt = $date->format('d-m-Y H:i:s').' '. utf8_encode($message)."\n";
-        $txt.= '===================================='."\n";
-        fwrite($file, $txt);
-        fclose($file);
+    public static function qLog($message){
+        EasyLogger::instantiate();
+        EasyLogger::$logger->log($message);
+    }
+
+    public static function quickLogCaller(){
+        EasyLogger::instantiate();
+        $bt = debug_backtrace();
+        $bt = array_shift($bt);
+        $caller = array_shift($bt);
+
+        EasyLogger::$logger->log(print_r($caller, true));
+    }
+
+    public function logThisVariable($var){
+        $this->log(var_export($var, true));
+    }
+
+    public function serializeThisVariable($var){
+        $this->log(serialize($var));
     }
 
     public function log($message){
-        //
-        $date = new DateTime('now');
-        $txt = $date->format('d-m-Y H:i:s').' '.$message."\n";
-        $txt.= '===================================='."\n";
-        fwrite($this->file, $txt);
+        if(is_string($message)) {
+            $date = new DateTime('now');
+            $txt = $date->format('d-m-Y H:i:s') . ' ' . $message . "\n";
+            fwrite($this->file, $txt);
+        }
+        else
+            $this->log(var_export($message, true));
+        return;
     }
 
     public function __destruct(){
         fclose($this->file);
     }
+}
+
+function ecLog($message){
+    EasyLogger::quickLog($message);
 }
